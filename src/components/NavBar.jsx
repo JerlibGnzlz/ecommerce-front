7b113b03efed591ad633233eb0a11c30e43aea2c
Pedro <pedrocastillo24.pc@gmail.com>
2022-06-23 11:42:13 -0300

    estilos del nav
    Se agrego el logo, redondearon los bordes de los botones, padding agregado.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 import {
   faLocationDot,
   faMagnifyingGlass,
   faShoppingCart,
 } from "@fortawesome/free-solid-svg-icons";
+import logo from "../img/logo.png";
 
 function NavBar() {
+  const [product, setProduct] = useState("");
+
+  const handleChange = (e) => {
+    setProduct(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="bg-primary text-white py-3 flex justify-around ">
-      <div>FREE STYLE</div>
+    <div className="bg-primary text-tertiary py-4 flex justify-around  ">
+      <div className="w-25 h-10 bg-black p-2 rounded shadow shadow-tertiary">
+        <img src={logo} alt="logo" className="h-7 w-28 bg-black" />
+      </div>
 
       <div>
         <FontAwesomeIcon icon={faLocationDot} className="mx-1" />
         Location
       </div>
 
-      <div>
-        <input type="search" className=" w-80 outline-0 text-black" />
-        <button className="bg-secondary  px-2">
+      <div className="text-black flex ">
+        <select className="outline-0 h-8 text-sm text-tertiary w-auto text-center rounded-l bg-secondary hover:cursor-pointer hover:text-white">
+          <option selected value="all">
+            All
+          </option>
+          <option value="mens">Mens</option>
+          <option value="women">Women</option>
+          <option value="kids">Kids</option>
+        </select>
+
+        <input
+          type="search"
+          value={product}
+          onChange={handleChange}
+          className=" w-80 outline-0 text-black px-1 h-8 "
+        />
+
+        <button
+          onSubmit={handleSubmit}
+          className="bg-secondary  px-3 h-8 rounded-r"
+        >
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
       </div>
 
       <div>Hello, Guest</div>
-      <div>
-        <FontAwesomeIcon icon={faShoppingCart} /> Cart
-      </div>
-      <button className="bg-secondary px-2">Log in</button>
+      <Link to="">
+        <div className="hover:text-white">
+          <FontAwesomeIcon icon={faShoppingCart} /> Cart
+        </div>
+      </Link>
+
+      <button className="bg-secondary px-3 rounded hover:text-white ">
+        Log in
+      </button>
     </div>
   );
 }
